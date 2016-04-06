415551cd9621e9db8c17206040e9b5d1ae2f26ad
David Hwang <aviddhwang@gmail.com>
2016-04-06 12:27:19 -0400

    insert tooltip-esque reminder instead of using placeholder text

--- comment.js
+++ comment.js
@@ -10,7 +10,19 @@ var observer = new MutationObserver(function(mutations, observer) {
         return;
       var commentEl = addedNode.firstElementChild;
       var commentBox = commentEl.querySelector("." + githubCommentClassName + " textarea");
-      commentBox.placeholder = "Provide a reference if you can!";
+
+      var referenceReminderHTML = '       \
+        <div class="comment-arrow-box"> \
+          <p class="reference-reminder">                             \
+          <span> \
+            <span class="why">Why? </span> \
+            Consider citing a <span class="reference">reference</span> to help your reviewee. \
+          </span> \
+        </p>\
+        </div>                            \
+      ';
+      commentBox.insertAdjacentHTML("afterend", referenceReminderHTML)
+      // commentBox.placeholder = "Provide a reference if you can!";
     });
   });
 });
